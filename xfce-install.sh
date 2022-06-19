b7eb822cc52210030a8b02de43905ea0bf78e794
Pedro Daniel Gaspar Abrantes <daniel.abrantes@gmail.com>
2022-06-19 04:43:42 +0100

    Update xfce-install.sh

--- xfce-install.sh
+++ xfce-install.sh
@@ -11,21 +11,20 @@ fi
 cat ./xsessionrc >> /home/$SUDO_USER/.xsessionrc
 chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.xsessionrc
 
-apt install -y \
-    libxfce4ui-utils \
-    thunar \
-    xfce4-appfinder \
-    xfce4-panel \
-    xfce4-pulseaudio-plugin \
-    xfce4-whiskermenu-plugin \
-    xfce4-session \
-    xfce4-settings \
-    xfce4-terminal \
-    xfconf \
-    xfdesktop4 \
-    xfwm4 \
-    adwaita-qt \
-    qt5ct 
+apt install -y libxfce4ui-utils
+apt install -y thunar
+#apt install -y xfce4-appfinder
+apt install -y xfce4-panel
+apt install -y xfce4-pulseaudio-plugin
+#apt install -y xfce4-whiskermenu-plugin
+apt install -y xfce4-session
+apt install -y xfce4-settings
+apt install -y xfce4-terminal
+apt install -y xfconf
+apt install -y xfdesktop4
+apt install -y xfwm4
+apt install -y adwaita-qt
+apt install -y qt5ct 
 
 echo 
 echo xfce install complete, please reboot and issue 'startx'
